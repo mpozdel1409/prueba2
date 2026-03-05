35e68d518beaa691eed964f18a8797c54a4c7d42
alumno <alumno@alumno-Lenovo-V14-G5-IRL>
2026-03-05 10:39:12 +0100

    d

--- Prueba/src/Paquete/Clase.java
+++ Prueba/src/Paquete/Clase.java
@@ -3,8 +3,10 @@ package Paquete;
 public class Clase {
 
 	public static void main(String[] args) {
-		System.out.print("hola");
+		System.out.print("ho");
 		System.out.print("hol");
+		System.out.print("branch2");
+		
 		
 	}
 
